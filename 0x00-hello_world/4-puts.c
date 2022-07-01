73a55a2f11fec14c59339288d23f34f7e8561186
ikeiyke <ikeiyke22@gmail.com>
2022-07-01 00:31:14 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: 0
@@ -11,9 +10,6 @@
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
