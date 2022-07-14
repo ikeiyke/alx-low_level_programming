15744ecef519c446c453f5496372cd059b13b7ce
ikeiyke <ikeiyke22@gmail.com>
2022-07-14 08:40:21 -0700

    fix it

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
 include "main.h"
 /**
- * _strncat - concatenates two strings
- * @src: The source of strings
- * @dest: The destination of the string
- * @n: The length of int
- * Return: pointer to the resulting string dest
+ *_strncat - concatenates two strings
+ *@src: The source of strings
+ *@dest: The destination of the string
+ *@n: The length of int
+ *Return: pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
